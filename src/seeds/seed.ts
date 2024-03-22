e21c562f6753e547f99e50d39e22c0851122a694
Rashed Iqbal <dev.rashediqbal@gmail.com>
2024-03-23 00:10:34 +0600

    on the go

--- src/seeds/seed.ts
+++ src/seeds/seed.ts
@@ -1,37 +1,12 @@
 // prisma/seed.ts
 
 import { PrismaClient } from "@prisma/client";
-import { hashSync } from "bcrypt";
-
-const SEED_ADMIN_DATA = [
-  {
-    name: "Root User",
-    email: "rootuser@gmail.com",
-    password: "parasite",
-  },
-];
 
 // initialize Prisma Client
 const prisma = new PrismaClient();
 
 async function main() {
   // seeding data
-
-  const adminSeedData = SEED_ADMIN_DATA.map((v) => ({
-    ...v,
-    password: hashSync(v.password, 10),
-  }));
-
-  prisma.admin
-    .createMany({
-      data: adminSeedData,
-    })
-    .then((v) => {
-      console.log(v);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
 }
 
 // execute the main function
